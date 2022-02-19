410d1f7a281ef59855f2f10962783b22d26660de
Gabriela Papin <gabrielapapin20@gmail.com>
2022-02-18 21:30:29 -0300

    requisito 4 concluido

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends React.Component {
     return (
       <div>
         <Header />
-        <SolarSystem />
-        <Title headline="Planetas" />
+        <SolarSystem>
+          <Title />
+        </SolarSystem>
       </div>
     );
   }
